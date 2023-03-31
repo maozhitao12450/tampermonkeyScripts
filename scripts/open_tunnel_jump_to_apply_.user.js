cfa0b975dee0a8710921e2aa9170a282eb939c31
zhitao.mao <zhitao.mao@zhaopin.com.cn>
2023-03-31 17:31:57 +0800

    init

---
+++ scripts/open_tunnel_jump_to_apply_.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         跳转到申请页面
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       zhitao.mao
+// @match        http://opentunnel.net/v2ray/
+// @grant        none
+// @updateURL    https://gitee.com/naozeizei/tampermonkey/raw/master/scripts/open_tunnel_jump_to_apply_.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var spans = document.querySelectorAll("body > main > div:nth-child(13) > div > div > div > div.my-2.text-center.border-3.border-top.border-white > span")
+    for (const span of spans) {
+        var numbers =span.textContent.trim().split("/")
+        if (Number(numbers[0]) < Number(numbers[1])){
+            span.parentElement.parentElement.querySelector("a").click()
+            break
+        }
+    }
+})();
---
+++ scripts/opentunnel_jump_to_create.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         创建账号并复制到剪贴板
+// @namespace    http://tampermonkey.net/
+// @version      0.3
+// @description  try to take over the world!
+// @author       zhitao.mao
+// @match        http://opentunnel.net/v2ray/server-*
+// @grant        none
+// @updateURL    https://gitee.com/naozeizei/tampermonkey/raw/master/scripts/opentunnel_jump_to_create.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.querySelector("#username").value = Math.random().toString(36).slice(-8)
+    document.querySelector("#bh").value = "www.bing.com"
+
+    var interval1 = setInterval(function(){
+    var text = document.querySelector("body > main > div.row.g-2 > div.col-md-6 > div:nth-child(2) > button")
+    if (text){
+        text.click()
+        clearInterval(interval1)
+    }
+    }, 100);
+    var interval = setInterval(function(){
+    var text = document.querySelector("#copyhttp")
+        if (text){
+            text.click()
+            clearInterval(interval)
+            alert("复制完成")
+        }
+    }, 100);
+
+})();
